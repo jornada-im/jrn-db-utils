f437a8e09034b13d26acb9fb11e11fcc690476ed
Gregory E. Maurer <gregmaurer@gmail.com>
2021-05-03 13:03:14 -0600

    aws credential

--- jrn_cred_template.R
+++ jrn_cred_template.R
@@ -10,3 +10,10 @@ host = "<servername or ip>",
 port = 5432,
 user = '<username>',
 password = '<password>')
+
+#set the environmental variables for the jrn-data-entities bucket on GCS
+Sys.setenv("AWS_S3_ENDPOINT" = "storage.googleapis.com",
+           "AWS_ACCESS_KEY_ID" = "<aws_accesskeyid>",
+           "AWS_SECRET_ACCESS_KEY" = "<aws_secretaccesskey>",
+           "AWS_DEFAULT_REGION" = "us-west4",
+           "AWS_SESSION_TOKEN" = "mytoken")
